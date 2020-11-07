00f6acdfb38cc9551b5a024657daee9fa5ce5839
victor Naranjo <victornar97@gmail.com>
2020-11-07 15:48:40 -0400

    added link home

--- client/src/Components/Navbar/Logo.js
+++ client/src/Components/Navbar/Logo.js
@@ -1,24 +1,27 @@
 import React, { useContext } from "react";
 import IconLogo from "../Icons/IconLogo";
 import ThemeContext from "../../Context/Theme";
+import { Link } from "react-router-dom";
 import "./scss/logo.scss";
 const Logo = () => {
   const theme = useContext(ThemeContext);
 
   return (
-    <div className="container-logo">
-      <div className="logo">
-        <IconLogo
-          width="25"
-          heigth="25"
-          color={`${theme.dark ? "#EEE" : "#080412"}`}
-        />
+    <Link to="/">
+      <div className="container-logo">
+        <div className="logo">
+          <IconLogo
+            width="25"
+            heigth="25"
+            color={`${theme.dark ? "#EEE" : "#080412"}`}
+          />
+        </div>
+        <div className={`text-logo ${theme.dark ? "logo-dark" : ""}`}>
+          <p className="name">VICTOR NARANJO</p>
+          <p className="info">Desarrollador Web</p>
+        </div>
       </div>
-      <div className={`text-logo ${theme.dark ? "logo-dark" : ""}`}>
-        <p className="name">VICTOR NARANJO</p>
-        <p className="info">Desarrollador Web</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
